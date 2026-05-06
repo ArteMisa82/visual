eb7ca7e09bd90bb5bd7fd4fe9294d57406a5fe1b
CarlangasBeja <carlangasbeja@gmail.com>
2026-05-06 16:08:02 -0500

    Se creo clase PruebaXD ssss

--- src/practicagit2/ClasePruebaXD.java
+++ src/practicagit2/ClasePruebaXD.java
@@ -14,7 +14,7 @@ public class ClasePruebaXD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Clase XD
     }
     
 }
